7a77f2be53e284b371f661d8e514a58b72b4ca56
apple <apple@MacBook-Pro.local>
2025-05-27 23:37:15 +0530

    feat:removed extra line break

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -9,7 +9,6 @@ export const protect = async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.id).select('-password');
     if (!req.user) return res.status(401).json({ message: 'User not found' });
-
     next();
   } catch (err) {
     res.status(401).json({ message: 'Token is not valid' });
